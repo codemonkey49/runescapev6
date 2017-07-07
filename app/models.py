3cde040ac59a921c02321549fd600c7151f34d32
basedtoaster <edson@smith.net>
2017-07-07 19:02:45 +0000

    tweaks to make heroku work

--- app/models.py
+++ app/models.py
@@ -1,11 +1,11 @@
 from __future__ import unicode_literals
-
+from django.utils import timezone
 from django.db import models
 
 # Create your models here.
 class itemDataPoint(models.Model):
     id = models.BigIntegerField(primary_key = True)
-    itemID=models.IntegerField()
-    date=models.DateTimeField()
-    price=models.IntegerField()
+    itemID=models.IntegerField(default=0)
+    date=models.DateTimeField(default=timezone.now())
+    price=models.IntegerField(default=0)
     
