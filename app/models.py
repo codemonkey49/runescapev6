01656b9992e117c0f87296f4ecbef34f04d303c8
basedtoaster <edson@smith.net>
2017-07-07 19:24:47 +0000

    heroku tweaks

--- app/models.py
+++ app/models.py
@@ -4,7 +4,6 @@ from django.db import models
 
 # Create your models here.
 class itemDataPoint(models.Model):
-    id = models.BigIntegerField(primary_key = True)
     itemID=models.IntegerField(default=0)
     date=models.DateTimeField(default=timezone.now())
     price=models.IntegerField(default=0)
